e36136bf0e31df53a32327bc34c2e1e3a6c26ef6
David A. Chentes <davidalejandrochentes@gmail.com>
2023-11-17 11:19:40 -0500

    avance en app area

--- SGE_area/urls.py
+++ SGE_area/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('area/', views.area, name="area")
+    path('area/', views.area, name="area"),
+    path('alertas/', views.alertas, name="area_alertas"),
+    path('tabla_mantenimientos/', views.tabla_mantenimientos, name="area_tabla_mantenimientos"),
 ]
--- SGE_area/views.py
+++ SGE_area/views.py
@@ -1,5 +1,52 @@
 from django.shortcuts import render
+from .models import Area, TipoMantenimientoArea, MantenimientoArea
 
 # Create your views here.
 def area(request):
-    return render(request, 'SGE_area/area.html', {})
+    alert = Area.objects.all()
+    areas = Area.objects.filter(nombre__icontains=request.GET.get('search', ''))
+    total_areas = len(areas)
+    alertas = []
+    for area in alert:
+        dias_restantes = area.dias_restantes_mantenimiento()
+        if dias_restantes <= 7:
+            
+            alertas.append({
+                'area': area,
+                'dias_restantes': dias_restantes
+            })
+    alertas_ordenadas = sorted(alertas, key=lambda x: x['dias_restantes'])
+    total_alertas = len(alertas_ordenadas)
+    context = {
+        'areas': areas,
+        'total_areas': total_areas,
+        'alertas': alertas_ordenadas,
+        'total_alertas': total_alertas,
+    }
+    return render(request, 'SGE_area/area.html', context)
+
+def alertas(request):
+    alert = Area.objects.all()
+    alertas = []
+    for area in alert:
+        dias_restantes = area.dias_restantes_mantenimiento()
+        if dias_restantes <= 7:
+            
+            alertas.append({
+                'area': area,
+                'dias_restantes': dias_restantes
+            })
+    alertas_ordenadas = sorted(alertas, key=lambda x: x['dias_restantes'])
+    total_alertas = len(alertas_ordenadas)
+    context = {
+        'alertas': alertas_ordenadas,
+        'total_alertas': total_alertas,
+    }
+    return render(request, 'SGE_area/alertas.html', context)
+
+def tabla_mantenimientos(request):
+    areas = Area.objects.all()
+    context = {
+        'areas': areas
+    }
+    return render(request, 'SGE_area/tablas.html', context)
