d8c4a07ed9331a1379431a3f4db6c923106b0d03
David A. Chentes <davidalejandrochentes@gmail.com>
2023-11-25 13:17:02 -0500

    al fin termine, solo falta corregir el 404

--- SGE/views.py
+++ SGE/views.py
@@ -11,12 +11,12 @@ def index(request):
 
 def log_in(request):
     if request.method =='GET':
-        return render(request, 'SGE/login.html', {'form': AuthenticationForm})
+        return render(request, 'SGE/log.html', {'form': AuthenticationForm})
     else:
         user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
             messages.success(request, "El usuario no existe, o la password es incorrecta")
-            return render(request, 'SGE/login.html', {'form': AuthenticationForm})
+            return render(request, 'SGE/log.html', {'form': AuthenticationForm})
         else:
             login(request, user)
             return redirect('index')
