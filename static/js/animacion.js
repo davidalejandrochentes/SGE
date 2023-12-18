115d0a748302b693d24aee05736ab4f9c51ba246
David A. Chentes <davidalejandrochentes@gmail.com>
2023-12-18 17:25:24 -0500

    fin de areas

--- static/js/animacion.js
+++ static/js/animacion.js
@@ -1,19 +1,19 @@
 window.sr = ScrollReveal();
 
 sr.reveal('.malla', {
-    duration: 4000,
+    duration: 3000,
     origin: 'left',
     distance: '-250px',
 });
 
 sr.reveal('.flor', {
-    duration: 4000,
+    duration: 3000,
     origin: 'top',
     distance: '-250px',
 });
 
 sr.reveal('.soga', {
-    duration: 4000,
+    duration: 3000,
     origin: 'rign',
     distance: '-250px',
 });
@@ -21,4 +21,56 @@ sr.reveal('.soga', {
 sr.reveal('.logo-portada', {
     duration: 4000,
     
+});
+
+sr.reveal('.flecha-atras', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px',
+});
+
+sr.reveal('.mas', {
+    duration: 3000,
+});
+
+sr.reveal('.tabla', {
+    duration: 4000,
+});
+
+sr.reveal('.alert', {
+    duration: 5000,
+});
+
+sr.reveal('.card', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px',
+});
+
+sr.reveal('.cabecera', {
+    duration: 3000,
+    origin: 'butoon',
+    distance: '-100px',
+});
+
+sr.reveal('.imagen-detalle', {
+    duration: 3000,
+    origin: 'butoon',
+    distance: '-100px',
+});
+
+sr.reveal('.descargar', {
+    duration: 1000,
+});
+
+sr.reveal('.mantenimineto', {
+    duration: 2500,
+});
+
+sr.reveal('.editar', {
+    duration: 4000,
+});
+
+sr.reveal('.eliminar', {
+    duration: 5500,
 });
