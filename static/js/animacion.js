1a83aaf4541b036df6556b5d6657a7114fa2e66e
David A. Chentes <davidalejandrochentes@gmail.com>
2023-12-18 00:48:49 -0500

    trabajo con animaciones

---
+++ static/js/animacion.js
@@ -0,0 +1,30 @@
+window.sr = ScrollReveal();
+
+sr.malla('.titulo', {
+    duration: 4000,
+    origin: 'bottom',
+    distance: '-200px',
+});
+
+sr.reveal('.sub', {
+    duration: 4000,
+    origin: 'top',
+    distance: '-200px',
+});
+
+sr.reveal('.linea', {
+    duration: 2500,
+    
+});
+
+sr.reveal('.bala-iz', {
+    duration: 4000,
+    origin: 'rign',
+    distance: '-300px',
+});
+
+sr.reveal('.bala-de', {
+    duration: 4000,
+    origin: 'left',
+    distance: '-300px',
+});
