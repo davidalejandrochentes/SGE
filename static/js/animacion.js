0d22d40e0a5406ef4482b8ca50c6d57d5031d8e3
David A. Chentes <davidalejandrochentes@gmail.com>
2023-12-18 00:55:21 -0500

    entramos en animaciones de area

--- static/js/animacion.js
+++ static/js/animacion.js
@@ -1,30 +1,24 @@
 window.sr = ScrollReveal();
 
-sr.malla('.titulo', {
-    duration: 4000,
-    origin: 'bottom',
-    distance: '-200px',
-});
-
-sr.reveal('.sub', {
-    duration: 4000,
-    origin: 'top',
-    distance: '-200px',
-});
-
-sr.reveal('.linea', {
-    duration: 2500,
-    
-});
-
-sr.reveal('.bala-iz', {
-    duration: 4000,
-    origin: 'rign',
-    distance: '-300px',
-});
-
-sr.reveal('.bala-de', {
+sr.reveal('.malla', {
     duration: 4000,
     origin: 'left',
-    distance: '-300px',
+    distance: '-250px',
+});
+
+sr.reveal('.flor', {
+    duration: 4000,
+    origin: 'top',
+    distance: '-250px',
+});
+
+sr.reveal('.soga', {
+    duration: 4000,
+    origin: 'rign',
+    distance: '-250px',
+});
+
+sr.reveal('.logo-portada', {
+    duration: 4000,
+    
 });
