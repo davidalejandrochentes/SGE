89f48dfcc926ae3fa079297ca1a97d8db6631c94
David A. Chentes <davidalejandrochentes@gmail.com>
2024-01-17 18:22:04 -0500

    modificaciones de las animaciones de entrada

--- static/js/animacion.js
+++ static/js/animacion.js
@@ -1,23 +1,5 @@
 window.sr = ScrollReveal();
 
-sr.reveal('.malla', {
-    duration: 3000,
-    origin: 'left',
-    distance: '-250px',
-});
-
-sr.reveal('.flor', {
-    duration: 3000,
-    origin: 'top',
-    distance: '-250px',
-});
-
-sr.reveal('.soga', {
-    duration: 3000,
-    origin: 'rign',
-    distance: '-250px',
-});
-
 sr.reveal('.logo-portada', {
     duration: 4000,
     
