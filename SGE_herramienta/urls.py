0fc68ce7928d3d922701d0b715679384a1694aab
David A. Chentes <davidalejandrochentes@gmail.com>
2024-03-26 17:22:44 -0400

    fin de herramientas

--- SGE_herramienta/urls.py
+++ SGE_herramienta/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('detalles/<int:id>', views.detalles, name="detalles_herramienta"),
     path('delete/<int:id>', views.eliminar, name="eliminar_herramienta"),
     path('delete_mantenimiento/<int:id>', views.eliminar_mantenimiento, name="eliminar_mantenimiento_herramienta"),
+    path('generar_documento_mantenimientos_por_mes/', views.generar_documento_mantenimientos_por_mes, name='generar_documento_mantenimientos_general_herramienta'),
+    path('generar_documento_mantenimientos_herramienta/<int:id>/', views.generar_documento_mantenimientos_herramienta, name='generar_documento_mantenimientos_herramienta'),
 ]
